9f5da8d93727f864eb1cc0f38894b5fc29ff36e4
Logan <logannaranjo4@gmail.com>
2024-11-13 20:18:19 +0100

    V1 | Pong Game

--- Manipulacion DOM/Pong/src/script.js
+++ Manipulacion DOM/Pong/src/script.js
@@ -10,6 +10,14 @@
     const bntPause = document.querySelector(".btnPause");
     const btnHome = document.querySelector(".btnHome");
     const btnConfirmStg = document.querySelector("#btnConfirmStn");
+    const keyChanger = document.querySelector("#keyChanger")
+    const musicIntro = document.getElementById('intro');
+    const stopMusic = document.querySelector(".stopMusic");
+    const svgMusicON = document.querySelector(".musicIconON")
+    const svgMusicOFF = document.querySelector(".musicIconOFF")
+
+    musicIntro.volume = 0.20; // Ajusta el valor según la necesidada
+
     
     var gameOn = false; //Indicamos que el no ha empezado
     let moveUPLeft = false;
@@ -20,26 +28,28 @@
 
     let settingsShow = false;
     let twoPlayers = false;
-
+    let musicOn = true;
 
 
     var ball_movement = null;
+    let velocityBall = 5;
     var velocity_x = null;
     var velocity_y = null;
+    
 
     //Caracteristicas del tablero
-    let widthBoard = 1800;
-    let heightBoard = 800;
+    let widthBoard = 95;
+    let heightBoard = 80;
     let borderBoard = 10;
-    let colorBorderBoard = "#b1b1b1";
-    let backgroundColorBoard = "#1A1A2E";
+    let colorBorderBoard = "white";
+    let ColorBoard = "#000000";
 
     applySettingsBoard = () =>{
     //configuraciones del tablero
-    gameBoard.style.width = widthBoard + "px"
-    gameBoard.style.height = heightBoard + "px"
+    gameBoard.style.width = widthBoard + "%";
+    gameBoard.style.height = heightBoard + "%";
     gameBoard.style.border = borderBoard + "px " + "solid " + colorBorderBoard; 
-    gameBoard.style.backgroundColor = backgroundColorBoard;
+    gameBoard.style.backgroundColor = ColorBoard;
     }
 
 
@@ -49,13 +59,14 @@
     //Calculo de radio de la pelota
     let radiusBall = ballSize/2;
 
-
     applySettingsBall = () =>{
     //configuraciones de la pelota
     ball.style.width = ballSize + "px";
     ball.style.height = ballSize + "px";
     ball.style.borderRadius = radiusBall + "px";
     ball.style.backgroundColor = ballColor;
+    velocity_x = velocityBall;
+    velocity_y = velocityBall;
     }
 
     //Caracteristicas de la pala
@@ -63,6 +74,12 @@
     let paddleHeight = 150;
     let separationPaddle = 50;
     let colorPaddle = "#ECECEC";
+
+    //Teclas por defecto
+    let j1Up = "a";
+    let j1Down = "z";
+    let j2Up = "k";
+    let j2Down = "m";
 
 
     applySettingsPaddleLeft = () =>{
@@ -79,6 +96,38 @@
         paddleRight.style.right = separationPaddle + "px"
         paddleRight.style.backgroundColor = colorPaddle;
     }
+
+    
+    //Configuraciones (Inputs)
+    let ballSizeStn = document.querySelector("#ballSizeStn");
+    let ballVelocityStn = document.querySelector("#ballVelocityStn");
+    let ballColorStn = document.querySelector("#colorPickerBall")
+
+    let paddleHeightStn = document.querySelector("#paddleHeightStn");
+    let paddleWidthStn = document.querySelector("#paddleWidthStn");
+    let paddleColorStn = document.querySelector("#colorPickerPaddle");
+
+    let keyJ1Up = document.querySelector(".J1Up");
+    let keyJ1Down = document.querySelector(".J1Down");
+    let keyJ2Up = document.querySelector(".J2Up");
+    let keyJ2Down = document.querySelector(".J2Down");
+
+    let boardColorStn = document.querySelector("#colorPickerBoard");
+
+    ballSizeStn.value = ballSize;
+    ballVelocityStn.value = velocityBall;
+    ballColorStn.value = ballColor;
+
+    paddleHeightStn.value = paddleHeight;
+    paddleWidthStn.value = paddleWidth;
+    paddleColorStn.value = colorPaddle;
+
+    keyJ1Up.textContent = j1Up;
+    keyJ1Down.textContent = j1Down;
+    keyJ2Up.textContent = j2Up;
+    keyJ2Down.textContent = j2Down;
+
+    boardColorStn.value = ColorBoard;
 
 
     //Aplciar configuracion por defecto de los elemetos del videojuego
@@ -144,6 +193,34 @@
         paddleRight.classList.remove("hiddenContent");
     }
 
+    hiddeKeyChanger = () =>{
+        keyChanger.classList.add("hiddenContent");
+    }
+
+    showKeyChanger = () =>{
+        keyChanger.classList.remove("hiddenContent");
+    }
+
+    showIconMusicON = () =>{
+        svgMusicON.classList.remove("hiddenContent");
+    }
+
+    hiddeIconMusicON = () =>{
+        svgMusicON.classList.add("hiddenContent");
+    }
+
+    showIconMusicOFF = () =>{
+        svgMusicOFF.classList.remove("hiddenContent");
+    }
+
+    hiddeIconMusicOFF = () =>{
+        svgMusicOFF.classList.add("hiddenContent");
+    }
+
+
+
+
+
     gameOverLayer = () =>{
         var playAgain = document.querySelector(".playAgain")
         var backHome = document.querySelector(".backMenu")
@@ -165,8 +242,8 @@
     restartPositionBall = () =>{
         ball.style.top = "calc(50% - 40px)";
         ball.style.left = "calc(50% - 40px)";
-        velocity_x = 5;
-        velocity_y = 5;
+        velocity_x = velocityBall;
+        velocity_y = velocityBall;
     }
 
     restartPositionPaddleLeft = () =>{
@@ -178,6 +255,7 @@
     }
 
     restartGame = () =>{
+        clearInterval(ball_movement);
         gamePaused = false;
         restartPositionBall();
         ball_movement = setInterval(move_ball, 10);
@@ -199,8 +277,6 @@
     gameTwoPlayers.addEventListener("click", () =>{
         gameOn = true;
         twoPlayers = true;
-
-        
         hiddeMenu();
         showGame();
         restartGame();
@@ -233,17 +309,58 @@
         clearInterval(ball_movement);
     })
 
-    btnConfirmStg.addEventListener("click", () =>{
-        let ballSizeStn = document.querySelector("#ballSizeStn");
-        let paddleHeightStn = document.querySelector("#paddleHeightStn");
-        let paddleWidthStn = document.querySelector("#paddleWidthStn");
+    // Función genérica para cambiar la tecla
+    const setKey = (element) => {
+        if (showSettings) {
+            showKeyChanger();
+            
+            // Define la función para el evento keypress
+            const keyPress = (event) => {
+                element.textContent = event.key;
+                // Remueve el evento una vez que se presione una tecla
+                document.removeEventListener("keypress", keyPress);
+                hiddeKeyChanger();
+            };
 
-        ballSize = Number(ballSizeStn.value ? Number(ballSizeStn.value) : 80);
-        paddleHeight = Number(paddleHeightStn.value) ? Number(paddleHeightStn.value) : 150;
-        paddleWidth = Number(paddleWidthStn.value) ? Number(paddleWidthStn.value) : 40;
+            // Agrega el evento keypress
+            document.addEventListener("keypress", keyPress);
+        }
+    };
+
+    // Asigna eventos a cada tecla
+    keyJ1Up.addEventListener("click", () => setKey(keyJ1Up));
+    keyJ1Down.addEventListener("click", () => setKey(keyJ1Down));
+    keyJ2Up.addEventListener("click", () => setKey(keyJ2Up));
+    keyJ2Down.addEventListener("click", () => setKey(keyJ2Down));
+
+
+    btnConfirmStg.addEventListener("click", () =>{
+
+        btnConfirmStg.style = "transform: scale(0.8)"
+
+        setTimeout(function(){
+            btnConfirmStg.style = "transform: scale(1)"
+        }, 150);
+
+
+        ballSize = (ballSizeStn.value) ? Number(ballSizeStn.value) : ballSize;
+        velocityBall = ballVelocityStn.value ? Number(ballVelocityStn.value) : velocityBall;
+        ballColor = ballColorStn.value ? ballColorStn.value : ballColor;
         radiusBall = ballSize/2;
 
+        paddleHeight = (paddleHeightStn.value) ? Number(paddleHeightStn.value) : paddleHeight;
+        paddleWidth = (paddleWidthStn.value) ? Number(paddleWidthStn.value) : paddleWidth;
+        colorPaddle = paddleColorStn.value ? paddleColorStn.value : paddleColor;
+
+        j1Up = keyJ1Up.textContent ? keyJ1Up.textContent : j1Up;
+        j1Down = keyJ1Down.textContent ? keyJ1Down.textContent : j1Down;
+        j2Up = keyJ2Up.textContent ? keyJ2Up.textContent : j2Up;
+        j2Down = keyJ2Down.textContent ? keyJ2Down.textContent : j2Down;
+
+        ColorBoard = boardColorStn.value ? boardColorStn.value : boardColor;
+
         applySettingsBall();
+        applySettingsBoard();
         applySettingsPaddleLeft();
         if (twoPlayers){
             applySettingsPaddleLeft();
@@ -252,7 +369,6 @@
     })
 
     bntPause.addEventListener("click", () =>{
-        console.log(gamePaused);
         if (!gamePaused){
             clearInterval(ball_movement)
             gamePaused = true;        
@@ -269,25 +385,21 @@
         gameOn = false;
     })
 
-
-    //Add onkeypress handler
-    //document.onkeydown = move_paddleLeft;
-
     document.addEventListener("keydown", (event) =>{
 
-        if(event.key == "a"){
+        if(event.key == j1Up){
             moveUPLeft = true;
         }
 
-        if (event.key == "z"){
+        if (event.key == j1Down){
             moveDownLeft = true;
         }
 
-        if(twoPlayers && event.key == "k"){
+        if(twoPlayers && event.key == j2Up){
             moveUPRight = true;
         }
 
-        if (twoPlayers && event.key == "m"){
+        if (twoPlayers && event.key == j2Down){
             moveDownRight = true;
         }
 
@@ -308,25 +420,38 @@
 
     document.addEventListener("keyup", (event) =>{
 
-        if(event.key == "a"){
+        if(event.key == j1Up){
             moveUPLeft = false;
         }
 
-        if (event.key == "z"){
+        if (event.key == j1Down){
             moveDownLeft = false;
         }
 
-        if(twoPlayers && event.key == "k"){
+        if(twoPlayers && event.key == j2Up){
             moveUPRight = false;
         }
 
-        if (twoPlayers && event.key == "m"){
+        if (twoPlayers && event.key == j2Down){
             moveDownRight = false;
         }
 
     })
 
-
+    stopMusic.addEventListener("click", () =>{
+        if (musicOn){
+            hiddeIconMusicON();
+            showIconMusicOFF();
+            musicIntro.pause();
+            musicIntro.currentTime = 0;
+            musicOn = false;
+        }else{
+            showIconMusicON();
+            hiddeIconMusicOFF();
+            musicIntro.play();
+            musicOn = true;
+        }
+    })
 
     // Move ball
     function move_ball(){
@@ -338,7 +463,6 @@
 
         //Check right
         if (ball.offsetLeft + (radiusBall * 2) >= (gameBoard.offsetWidth - borderBoard)) {
-            console.log(radiusBall);
             velocity_x = -velocity_x;
         }
 
@@ -346,8 +470,6 @@
         if ((ball.offsetTop + radiusBall * 2 ) >= (gameBoard.offsetHeight - borderBoard)) {
             velocity_y = -velocity_y;
         }
-
-
 
         //Check left - Game Lose
         if (ball.offsetLeft <= 0) {
@@ -375,30 +497,29 @@
 
     }
 
-    function move_paddleLeft(){
+    function move_paddle(){
 
         if (moveUPLeft && paddleLeft.offsetTop > 0){
             paddleLeft.style.top = (paddleLeft.offsetTop - 2) +"px";
-        } else if (moveDownLeft
-     && (paddleLeft.offsetTop + paddleLeft.offsetHeight) <= (gameBoard.offsetHeight - (borderBoard*2))){
+        } else if (moveDownLeft && (paddleLeft.offsetTop + paddleLeft.offsetHeight) <= (gameBoard.offsetHeight - (borderBoard*2))){
             paddleLeft.style.top = (paddleLeft.offsetTop + 2) +"px";
         }
 
-        requestAnimationFrame(move_paddleLeft);
-
-    }
-
-    function move_paddleRight(){
-
-        if (moveUPRight && paddleRight.offsetTop > 0){
+        if (twoPlayers && moveUPRight && paddleRight.offsetTop > 0){
             paddleRight.style.top = (paddleRight.offsetTop - 2) +"px";
-        } else if (moveDownRight && (paddleRight.offsetTop + paddleRight.offsetHeight) <= (gameBoard.offsetHeight - (borderBoard*2))){
+        } else if (twoPlayers && moveDownRight && (paddleRight.offsetTop + paddleRight.offsetHeight) <= (gameBoard.offsetHeight - (borderBoard*2))){
             paddleRight.style.top = (paddleRight.offsetTop + 2) +"px";
         }
 
-        requestAnimationFrame(move_paddleRight);
+        requestAnimationFrame(move_paddle);
 
     }
 
-    move_paddleLeft();
-    move_paddleRight();
+    move_paddle();
+
+
+    loseGame = () =>{
+        clearInterval(ball_movement)
+        showGameOver();
+        gameOverLayer();
+    }
