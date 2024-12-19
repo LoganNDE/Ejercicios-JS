84a8ac5828e8c37b6341e155c0c09cc6e24a4e9f
Logan <logannaranjo4@gmail.com>
2024-12-19 01:17:54 +0100

    Conecta4

--- Manipulacion DOM/conecta4/src/script.js
+++ Manipulacion DOM/conecta4/src/script.js
@@ -11,6 +11,8 @@ window.onload = () =>{
     let mensaje = document.getElementById("mensaje");
     let reiniciarBtn = document.querySelector(".reiniciar");
     let tablero;
+    let numeroTiradas = 0;
+    let ganador = false;
 
     //let matriz = Array.from({ length: filas }, () => new Array(columnas).fill(0));
     //let matriz = new Array(filas).fill(null).map(() => new Array(columnas).fill(0));
@@ -19,9 +21,10 @@ window.onload = () =>{
 
     generarMatriz = () => {
         mainMatrix = [];
-        for (let i = 0; i < filas; i++){
-            mainMatrix.push(new Array(columnas).fill(-1))
+        for (let i = 0; i < filas; i++) {
+            mainMatrix.push(new Array(columnas).fill(-1));
         }
+        mainMatrix.reverse(); // Invierte las filas
         console.log(mainMatrix);
         tablero = mainMatrix;
     };
@@ -33,24 +36,13 @@ window.onload = () =>{
         tableroDOM.style.gridTemplateColumns = `repeat(${columnas}, 1fr)`
         tableroDOM.style.gridTemplateRows = `repeat(${filas}, 1fr)`
         for (let i = (tamañoTablero-1); i >= 0 ; i--){
-            let celda = document.createElement("div");
-            celda.classList.add("celdaGrid");
-            celda.style.display = "flex";
-            celda.style.justifyContent = "center";
-            celda.style.alignItems = "center";
-
             let fichaVacia = document.createElement("div");
             fichaVacia.classList.add("ficha");
             fichaVacia.style.backgroundColor = "white";
             fichaVacia.setAttribute("id", i)
-            fichaVacia.style.height = "80%";
-            fichaVacia.style.width = "80%";
-            fichaVacia.style.borderRadius = "80%";
 
-            celda.appendChild(fichaVacia);
-            
+            tableroDOM.appendChild(fichaVacia);
 
-            tableroDOM.appendChild(celda);
         }
     }
 
@@ -69,26 +61,30 @@ window.onload = () =>{
             while (!casillaVacia){
                 if (tablero[contadorCasilla][filaMarcada] == -1){
                     casillaVacia = true;
+                    numeroTiradas++;
                     celdaMarcarDOM = document.getElementById(casillaDOM)
                     
                     if (jugador){
                         celdaMarcarDOM.style.backgroundColor = "red";
                         celdaMarcarDOM.style.border = "1.5px solid black";
-                        tablero[contadorCasilla][filaMarcada] = "PJ1";
+                        tablero[contadorCasilla][filaMarcada] = jugador;
                         jugador = false;
                     }else{
                         celdaMarcarDOM.style.backgroundColor = "yellow";
                         celdaMarcarDOM.style.border = "1.5px solid black"
-                        tablero[contadorCasilla][filaMarcada] = "PJ2";
+                        tablero[contadorCasilla][filaMarcada] = jugador;
                         jugador = true;
                     
                     }
+                    
+                    comprobarGanador(contadorCasilla, filaMarcada);
+                
                 }else{
                     contadorCasilla = contadorCasilla + 1;
                     casillaDOM = casillaDOM + 7;
                     if (tablero[contadorCasilla] == undefined){
                         mensaje.className = "";
-                        mensaje.classList.add("alerta")
+                        mensaje.classList.add("alerta");
                         mensaje.innerText = "Fila llena";
                         break;
                     }
@@ -102,16 +98,7 @@ window.onload = () =>{
         }
     }
 
-
-    borrarMensaje = setInterval(() =>{
-        if (mensaje.textContent != null){
-            mensaje.innerText = null;
-        }
-    }, 6000
-    )
-
-
-    reiniciarJuego = () =>{
+    const reiniciarJuego = () =>{
         todasLasCeldas = document.querySelectorAll(".ficha");
 
         todasLasCeldas.forEach(celda => {
@@ -120,14 +107,99 @@ window.onload = () =>{
         });
 
         generarMatriz();
+        tableroDOM.addEventListener("click", handleTirarFicha);
+        iniciarBorradoMensaje();
+        numeroTiradas = 0;
+        jugador = true;
 
         mensaje.className = "";
         mensaje.classList.add("exitoso")
         mensaje.innerText = "Tablero reiniciado";
     }
+    
+    
+    const comprobarVertical = (contadorFila, casillaMarcada) =>{
+        let contadorFichas = 0;
+        console.log("cords: " + contadorFila + ":" + casillaMarcada)
+
+        console.log(tablero[contadorFila][casillaMarcada])
+
+
+        //CAMBIAMOS DE JUGADOR DE FALSE->TRUE, YA QUE AL TIRAR LA FICHA, ESTA CAMBIA DE JUGADOR ANTES DE HACER LA COMPORBACIÓN
+        if (!jugador){
+            for (let i = casillaMarcada; tablero[contadorFila][i] == true; i++){
+                contadorFichas++;
+                if (contadorFichas == 4){
+                    alert("ganadorJ1")
+                    break;
+                }
+            }
+            contadorFichas = 0
+            for (let i = casillaMarcada; tablero[contadorFila][i] == true; i--){
+                contadorFichas++;
+                if (contadorFichas == 4){
+                    alert("ganadorJ1")
+                    break;
+                }
+            }
+        }else{
+            for (let i = casillaMarcada; tablero[contadorFila][i] == false; i++){
+                contadorFichas++;
+                if (contadorFichas == 4){
+                    alert("ganadorJ2")
+                    break;
+                }
+            }
+        }
+
+    }
 
 
 
-    tableroDOM.addEventListener("click", ()=> tirarFicha(event));
-    reiniciarBtn.addEventListener("click", reiniciarJuego)
+
+    const comprobarGanador = (contadorCasilla, filaMarcada) =>{
+        console.log(numeroTiradas);
+        if (numeroTiradas == tamañoTablero){
+            tableroDOM.removeEventListener("click", handleTirarFicha);
+            detenerBorradorMensaje();
+            mensaje.className = "";
+            mensaje.classList.add("error");
+            mensaje.innerText = "¡EMPATE! Reinicie el juego para jugar de nuevo";
+        }
+        
+        comprobarVertical(contadorCasilla, filaMarcada);
+        /*while (!ganador){
+            
+        
+        } 
+        
+        */
+        
+    }
+
+
+
+    function handleTirarFicha(event) {
+        tirarFicha(event);
+    }
+
+
+    iniciarBorradoMensaje = () =>{
+        borrarMensaje = setInterval(() =>{
+            if (mensaje.textContent != null){
+                mensaje.innerText = null;
+            }
+        }, 6000
+        )
+    }
+
+    iniciarBorradoMensaje();
+
+    detenerBorradorMensaje = () =>{
+        clearInterval(borrarMensaje);
+    }
+    
+
+    tableroDOM.addEventListener("click", handleTirarFicha);
+    reiniciarBtn.addEventListener("click", reiniciarJuego);
 }
